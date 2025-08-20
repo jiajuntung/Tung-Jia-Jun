d1d64b05d50334219cafa03c5a603afb08593a47
jiajuntung <jiajuntung@1utar.my>
2025-08-20 15:48:37 +0800

    Delete FlipGo (SecondHandPlatform) directory

--- FlipGo (SecondHandPlatform)/Config.tsx
+++
@@ -1,6 +0,0 @@
-exports.settings = {
-    // If you use actual device, can try to set it to the IP address of the server
-    // For emulator, 10.0.2.2 refers as the loopback address
-    // 127.0.0.1
-    serverPath: 'http://10.0.2.2:5000',
-}
--- FlipGo (SecondHandPlatform)/screens/LogoutScreen.tsx
+++
@@ -1,8 +0,0 @@
-import { Alert } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const logout = async (navigation: any) => {
-  Alert.alert('Logout', 'Successful Logout');
-  await AsyncStorage.removeItem('currentUser');
-  navigation.navigate('SignInSignUpScreen');
-};
